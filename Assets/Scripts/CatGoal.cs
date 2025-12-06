880b39cd850822388a197bad5c4e9c2ca6d45a9f
Josep Albareda <josepalbaredasolans@gmail.com>
2025-12-06 14:11:37 +0100

    collect and win mechanics

--- Assets/Scripts/CatGoal.cs
+++ Assets/Scripts/CatGoal.cs
@@ -4,16 +4,24 @@ public class CatGoalSimple : MonoBehaviour
 {
     public GameObject blackBG;   // Fondo
     public GameObject winImage;  // foto victoria
+    public bool catHasJewels;    // si el gato tiene joyas
+    [SerializeField] GameObject jewelsSprite; // sprite de las joyas
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Goal"))   
+        if (other.CompareTag("Goal") && catHasJewels)   
         {
             blackBG.SetActive(true);
             winImage.SetActive(true);
 
             // pausa el juego
             Time.timeScale = 0f;
+        }
+        if (other.CompareTag("Jewels") && !catHasJewels)
+        {
+            catHasJewels = true;
+            jewelsSprite.SetActive(true);
+            other.gameObject.SetActive(false);
         }
     }
 }
